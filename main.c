ca21531ba0587804b420cb3dde12c0be1910a479
matpw7 <84544977+matpw7@users.noreply.github.com>
2021-05-21 00:36:01 +0200

    Add files via upload
    fixed main

--- main.c
+++ main.c
@@ -3,28 +3,8 @@
 
 #include <stdio.h>
 
-
-
-//resistors values
-float res[] = { 1, 2.2, 4.7, 6.8, 10 };
-
-// newline depends on system
-#define NL printf("\r\n")
-
-float compute(float a, float b)
-{
-	return a*b/(a+b);
-}
-
-
-
-
-
-
-#define RESLEN (sizeof(res)/sizeof(float))
-
-
-
+#include "userParams.h"
+#include "tools.h"
 
 void main()
 {
@@ -48,13 +28,4 @@ void main()
 	}
 }
 
-//output:
-/*
-Resistors:
-        1.0    2.2    4.7    6.8   10.0  
- 1.0:   0.50   0.69   0.82   0.87   0.91  
- 2.2:   0.69   1.10   1.50   1.66   1.80  
- 4.7:   0.82   1.50   2.35   2.78   3.20  
- 6.8:   0.87   1.66   2.78   3.40   4.05  
-10.0:   0.91   1.80   3.20   4.05   5.00  
-*/
+
